330f9b70dd401d33971ad54fa6a8d328281da4ff
RolandWarburton <warburtonroland@gmail.com>
2019-12-03 21:44:33 +0000

    added concurrency

--- seekCounter.py
+++ seekCounter.py
@@ -1,17 +1,11 @@
-import sys
-import math
 import concurrent.futures
 import requests
 import threading
 from multiprocessing import cpu_count
-from itertools import product
-from itertools import repeat
 from functools import partial
-import requests
 import re
 from bs4 import BeautifulSoup
-from collections import OrderedDict 
-import include.job
+from collections import OrderedDict
 import time
 
 # ProcessPoolExecutor runs each of your workers in its own separate child process.
@@ -27,72 +21,63 @@ TGREEN =  '\033[32m'
 TWHITE = '\033[37m'
 TRED = '\033[91m'
 
-f = open('a.txt', 'w')
-
-def GetPageNumber(jobNum):
-    return math.floor(jobNum / 22)
-
 # Extract Job ids from html
-def ExtractJobIDs(content):
-    jobIDs = re.findall(r"job/[\d]{8}", str(content))
+def ExtractJobIDs(HTMLcontent):
+    jobIDs = re.findall(r"job/[\d]{8}", str(HTMLcontent))
     jobIDs = re.findall(r'\d+', str(jobIDs))
     jobIDs = list(OrderedDict.fromkeys(jobIDs)) 
     return jobIDs
 
-def SearchKeywords(jobID, languages):
+# look for languages. returns a list of languages
+def SearchKeywords(jobID, keyWords):
     posts = []
     soup = BeautifulSoup(downloadSite("https://www.seek.com.au/job/" + str(jobID)).content, "lxml")
-    for language in languages:
+    for language in keyWords:
         anchor = soup.find(['p', 'li', 'span', 'strong'], text=re.compile(re.escape(language), re.IGNORECASE))
         if anchor:
             # print("found " + str(language) + " in " + str(jobID))
             posts.append(str(language))
-    if posts: print(str(posts) + jobID)
     return posts
 
-# Return EVERY job ID. use with caution
-def GetJobIDs(page):
-    return NotImplemented
+# Return every job ID for a page
+def GetJobIDs(pageNumber, baseURL):
+    site = downloadSite(baseURL + "/?page=" + str(pageNumber))
+    jobIDs = ExtractJobIDs(site.content)
+    return jobIDs
 
 def downloadSite(url):
     requestHTTP = requests.get(url)
+    # print(f"download {requestHTTP}")
     return requestHTTP
 
 def UpdateTotalJobs(url):
     soup = BeautifulSoup(requests.get(url).content, "lxml")
     return int(' '.join(soup.find("strong", attrs={"data-automation":True})).replace(',', ''))
 
-
 def main():
     
     baseURL = "https://www.seek.com.au/jobs-in-information-communication-technology/in-Melbourne-CBD-&-Inner-Suburbs-Melbourne-VIC"
-    languages = ["javascript", "node", "c#", "css", "html", "Manager"]
+    # languages = ["javascript", "node", "c#", "css", "html"]
+    languages = ["1 year", "2+ years", "3+ years", "4+ years", "5+ years", "6+ years"]
     jobIDs = []
     posts = []
-    pageNumber = 1
 
     # totalPages = int(input(TRED + "Enter number of pages: " + TWHITE))
-    totalPages = 2
+    totalPages = 100
     start = time.time()
 
-    # supposed to be getting a batch of jobIDs from different pages
-    # for pageNum in range(totalPages):
-    #     GetJobIDs()
-    #     jobIDs.extend()
-    # with concurrent.futures.ProcessPoolExecutor() as executor:
-    #     result = executor.map(partial(ExtractJobIDs), baseURL + str(range(totalPages)))
-    #     # print(result)
-    #     # posts.extend(result)
+    # get a batch of jobIDs from multiple pages
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        result = executor.map(partial(GetJobIDs, baseURL = baseURL), range(totalPages))
+        jobIDs.extend(result)
 
-    while pageNumber < totalPages+1:
-        print(TGREEN + "Page Number " + str(pageNumber) + TWHITE)
-        url = baseURL + "?page=" + str(pageNumber)
-        content = requests.get(url).content
-        jobIDs = ExtractJobIDs(content)
-        with concurrent.futures.ProcessPoolExecutor() as executor:
-            result = executor.map(partial(SearchKeywords, languages=languages), jobIDs)
-            posts.extend(result)
-        pageNumber += 1
+    # process all the jobs
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        # page is a list of job IDs on a single page
+        for page in jobIDs:
+            result = executor.map(partial(SearchKeywords, languages=languages), page)
+            for languageArray in result:
+                posts.extend(languageArray)
 
     timeTaken = time.time()-start 
     print("==============================")
@@ -101,9 +86,7 @@ def main():
         print(str(language) + ": " + str(posts.count(language)))
     print(f"finished in {timeTaken} seconds")
     print("==============================")
-    
 
-    f.close()
 if __name__ == "__main__":
     main()
 
